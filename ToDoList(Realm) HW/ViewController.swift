305e499cf735b77c2387587f601ae41dd9f8f8f5
Sergey-Yurtaev <engineerk2@mail.ru>
2022-06-06 20:30:35 +0500

    Initial Commit

---
+++ ToDoList(Realm) HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList(Realm) HW
+//
+//  Created by Sergey Yurtaev on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
